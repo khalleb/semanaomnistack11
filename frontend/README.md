7b62bb005cab2f5f51a144cdc1d291eae8834c51
khalleb <khallebw@gmail.com>
2020-03-26 01:27:25 -0300

    criando fontend

---
+++ frontend/README.md
@@ -0,0 +1,8 @@
+- Iniciando o projeto
+$ npm start
+
+- Instalando pacote de fontes
+$ npm install react-icons
+
+- responsável por rotas
+$ npm install react-router-dom
---
+++ frontend/src/pages/Logon/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { FiLogIn } from 'react-icons/fi'
+
+import './styles.css'
+import logoImg from '../../assets/logo.svg'
+import heroesImage from '../../assets/heroes.png'
+
+export default function Logon() {
+  return (
+    <div className="logon-container">
+      <section className="form">
+        <img src={logoImg} />
+        <form>
+          <h1>Faça seu pedido</h1>
+
+          <input placeholder="Sua ID" />
+          <button className="button" type="submit">Entrar</button>
+
+          <a href="/register">
+            <FiLogIn size={16} color="#E02041"/>
+            Não tenho cadastro
+          </a>
+        </form>
+      </section>
+      <img src={heroesImage} alt="Heroes" />
+    </div>
+  )
+}
