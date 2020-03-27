fbc338726ec08c3238e19499f6b497126cadaf55
khalleb <khallebw@gmail.com>
2020-03-27 01:14:47 -0300

    Iniciando projeto

---
+++ mobile/README.md
@@ -0,0 +1,11 @@
+* Instalando expo
+$ npm install -g expo-cli
+
+* Criando projeto com expo
+$ expo init nomeProjeto
+
+* Bibliotecas
+- Para rotas - https://reactnavigation.org/docs/getting-started
+$ npm install @react-navigation/native
+$ expo install react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context @react-native-community/masked-view
+$ npm install @react-navigation/stack
---
+++ mobile/src/pages/Incidents/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View, Image, Text } from 'react-native';
+
+import logoImg from '../../assets/logo.png'
+import styles from './styles'
+export default function Incidents() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={logoImg} />
+        <Text style={styles.headerText}>
+          Total de <Text style={styles.headerTextBold}>O casos</Text>
+        </Text>
+        <Text style={styles.title}>Bem-vindo!</Text>
+        <Text style={styles.description}>Escolha um dos casos abaixo e salva</Text>
+      </View>
+    </View>
+  )
+}
